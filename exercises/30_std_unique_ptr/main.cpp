a755ba36c929b960cf86e820480fafce40d258fb
muyi <1109648848@qq.com>
2025-12-31 14:50:07 +0800

    all_correct

--- exercises/30_std_unique_ptr/main.cpp
+++ exercises/30_std_unique_ptr/main.cpp
@@ -19,15 +19,18 @@ public:
     ~Resource() {
         RECORDS.push_back(_records);
     }
+    //析构时会把recards记录下来
 };
 
 using Unique = std::unique_ptr<Resource>;
 Unique reset(Unique ptr) {
     if (ptr) ptr->record('r');
+    //会调用析构
     return std::make_unique<Resource>();
 }
 Unique drop(Unique ptr) {
     if (ptr) ptr->record('d');
+    //会调用析构
     return nullptr;
 }
 Unique forward(Unique ptr) {
@@ -39,12 +42,15 @@ int main(int argc, char **argv) {
     std::vector<std::string> problems[3];
 
     drop(forward(reset(nullptr)));
+    // reset（nullptr)不会记录r 因为时空 之后析构  之后forword记录f 最后drop记录d 记 fd
     problems[0] = std::move(RECORDS);
 
     forward(drop(reset(forward(forward(reset(nullptr))))));
+    //reset 空指针不记录r 两次forwar 记录 ff reset记录 ffr 就会写入因为rest会重新创建  d
     problems[1] = std::move(RECORDS);
 
     drop(drop(reset(drop(reset(reset(nullptr))))));
+    //reset 空指针不记录r  第二次的reset不是空记录记录 r 析构 d 就会d
     problems[2] = std::move(RECORDS);
 
     // ---- 不要修改以上代码 ----
@@ -53,8 +59,8 @@ int main(int argc, char **argv) {
         {"fd"},
         // TODO: 分析 problems[1] 中资源的生命周期，将记录填入 `std::vector`
         // NOTICE: 此题结果依赖对象析构逻辑，平台相关，提交时以 CI 实际运行平台为准
-        {"", "", "", "", "", "", "", ""},
-        {"", "", "", "", "", "", "", ""},
+        {"d", "ffr"},
+        {"d", "d", "r"},
     };
 
     // ---- 不要修改以下代码 ----
@@ -62,6 +68,7 @@ int main(int argc, char **argv) {
     for (auto i = 0; i < 3; ++i) {
         ASSERT(problems[i].size() == answers[i].size(), "wrong size");
         for (auto j = 0; j < problems[i].size(); ++j) {
+            // NOTICE: 这里使用 std::
             ASSERT(std::strcmp(problems[i][j].c_str(), answers[i][j]) == 0, "wrong location");
         }
     }
--- exercises/31_std_shared_ptr/main.cpp
+++ exercises/31_std_shared_ptr/main.cpp
@@ -8,38 +8,57 @@
 int main(int argc, char **argv) {
     auto shared = std::make_shared<int>(10);
     std::shared_ptr<int> ptrs[]{shared, shared, shared};
+    //共享指针  三个指针共享一个10个int的数组
 
     std::weak_ptr<int> observer = shared;
-    ASSERT(observer.use_count() == ?, "");
+    //弱引用不增加引用计数
+    ASSERT(observer.use_count() == 4, "");
+    //三个指针共享一个10个int的数组 所以引用计数为3
 
     ptrs[0].reset();
-    ASSERT(observer.use_count() == ?, "");
-
+    ASSERT(observer.use_count() == 3, "");
     ptrs[1] = nullptr;
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 2, "");
 
     ptrs[2] = std::make_shared<int>(*shared);
-    ASSERT(observer.use_count() == ?, "");
+    //会析构原来的引用让计数-1 
+    ASSERT(observer.use_count() == 1, "");
 
     ptrs[0] = shared;
     ptrs[1] = shared;
     ptrs[2] = std::move(shared);
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 3, "");
+    if (ptrs[0] == nullptr) {
+        std::cout << "ptrs[0] is nullptr" << std::endl;
+    } else {
+        std::cout << "ptrs[0] is not nullptr" << std::endl;
+    }
 
+    std::cout << observer.use_count() << std::endl;
     std::ignore = std::move(ptrs[0]);
+    //auto temp = std::move(ptrs[0]);
+        if (ptrs[0] == nullptr) {
+        std::cout << "ptrs[0] is nullptr" << std::endl;
+    } else {
+        std::cout << "ptrs[0] is not nullptr" << std::endl;
+    }
+    //从打印看这个ignore并没有改变 甚至没有让ptrs[0]变成nullptr
+    std::cout << observer.use_count() << std::endl;
     ptrs[1] = std::move(ptrs[1]);
+    std::cout << observer.use_count() << std::endl;
     ptrs[1] = std::move(ptrs[2]);
-    ASSERT(observer.use_count() == ?, "");
+    std::cout << observer.use_count() << std::endl;
+    ASSERT(observer.use_count() == 2, "");
 
     shared = observer.lock();
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 3, "");
 
     shared = nullptr;
     for (auto &ptr : ptrs) ptr = nullptr;
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 0, "");
 
     shared = observer.lock();
-    ASSERT(observer.use_count() == ?, "");
+    ASSERT(observer.use_count() == 0, "");
 
     return 0;
 }
